146efc608194a442236b2374af4417696928fbcd
Sergey Pashaev <bioh@yandex-team.ru>
2020-03-27 10:28:41 +0300

    cut evil-magit

--- modules/tools/magit/config.el
+++ modules/tools/magit/config.el
@@ -75,34 +75,3 @@
 
   ;; Close transient with ESC
   (define-key transient-map [escape] #'transient-quit-one))
-
-(use-package! evil-magit
-  :when (featurep! :editor evil +everywhere)
-  :after magit
-  :init
-  (setq evil-magit-state 'normal
-        evil-magit-use-z-for-folds t)
-  :config
-  (unmap! magit-mode-map
-    ;; Replaced by z1, z2, z3, etc
-    "M-1" "M-2" "M-3" "M-4"
-    "1" "2" "3" "4"
-    "0") ; moved to g=
-  (evil-define-key* 'normal magit-status-mode-map [escape] nil) ; q is enough
-  (evil-define-key* '(normal visual) magit-mode-map
-    "%"  #'magit-gitflow-popup
-    "zz" #'evil-scroll-line-to-center
-    "g=" #'magit-diff-default-context)
-  (define-key! 'normal
-    (magit-status-mode-map
-     magit-stash-mode-map
-     magit-revision-mode-map
-     magit-diff-mode-map)
-    [tab] #'magit-section-toggle)
-  (after! git-rebase
-    (dolist (key '(("M-k" . "gk") ("M-j" . "gj")))
-      (when-let (desc (assoc (car key) evil-magit-rebase-commands-w-descriptions))
-        (setcar desc (cdr key))))
-    (evil-define-key* evil-magit-state git-rebase-mode-map
-      "gj" #'git-rebase-move-line-down
-      "gk" #'git-rebase-move-line-up)))
--- modules/tools/magit/packages.el
+++ modules/tools/magit/packages.el
@@ -1,6 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; tools/magit/packages.el
 
-(when (package! magit :pin "55c5c7cb83")
-  (when (featurep! :editor evil +everywhere)
-    (package! evil-magit :pin "0b79aa33a4")))
+(package! magit :pin "55c5c7cb83")
