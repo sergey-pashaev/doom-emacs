f5b196c2530a6b1f73352e159453aa8e7c38bbfe
Sergey Pashaev <bioh@yandex-team.ru>
2020-03-29 16:09:27 +0300

    cut helm-describe-modes

--- modules/completion/helm/packages.el
+++ modules/completion/helm/packages.el
@@ -5,9 +5,6 @@
 (package! helm-rg :pin "785a80fe5c")
 (package! helm-c-yasnippet :pin "65ca732b51")
 (package! helm-company :pin "6eb5c2d730")
-(package! helm-describe-modes
-  :recipe (:host github :repo "emacs-helm/helm-describe-modes")
-  :pin "11fb36af11")
 (package! helm-projectile :pin "5328b74ddd")
 (package! swiper-helm :pin "93fb6db87b")
 (when (featurep! +childframe)
