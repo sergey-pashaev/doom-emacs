10ddecfb1e9b7e79d3973a351935521572e62ab4
Sergey Pashaev <bioh@yandex-team.ru>
2020-05-30 09:16:59 +0300

    add clang-format for repos w/o lsp support

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -30,6 +30,8 @@
 
 (package! cprg
   :recipe (:host github :repo "sergey-pashaev/cprg"))
+
+(package! clang-format)
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
 ;; `:files' in the `:recipe':
